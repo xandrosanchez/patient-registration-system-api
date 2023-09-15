e28aa53d8aa72933e500f5b351434bc36e82d967
xandrosanchez <din.myrk@gmail.com>
2023-09-15 16:10:57 +0300

    Initial commit

---
+++ src/main/java/com/example/patientregistrationsystemapi/PatientRegistrationSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.patientregistrationsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRegistrationSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRegistrationSystemApiApplication.class, args);
+	}
+
+}
